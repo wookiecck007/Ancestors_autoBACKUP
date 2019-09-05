1d27b10bc04d074b1dad60c62b59df47a52eef03
wookiecck007 <54890013+wookiecck007@users.noreply.github.com>
2019-09-05 12:16:14 +0200

    Update 1clickAncestors.bat

--- 1clickAncestors.bat
+++ 1clickAncestors.bat
@@ -1,1 +1,13 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wookiecck007/Ancestors_autoBACKUP/master/AncestorsBackup.ps1'))"
+@echo off
+%@Try%
+	@echo Downloading script.....
+	PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wookiecck007/Ancestors_autoBACKUP/master/AncestorsBackup.')) -ErrorVariable ProcessError; echo $ProcessError"
+%@EndTry%
+:@Catch
+	cls
+	echo Error
+	echo -------------------
+	echo Ensure internet conenctivity for downloading the script
+	echo -------------------
+	pause
+:@EndCatch
