8e903a0253b0736bb413d5e2e7018fe7c7f1bef0
wookiecck007 <54890013+wookiecck007@users.noreply.github.com>
2019-09-05 11:55:48 +0200

    New 1click file (always updated)
    This will make a 1 click - always updated bat file to start the game and start backup service. 
    (Requires Internet connectivity for latest PowerShell commands)

---
+++ 1clickAncestors.bat
@@ -0,0 +1,1 @@
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/wookiecck007/Ancestors_autoBACKUP/master/AncestorsBackup.ps1'))"
